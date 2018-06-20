1ace53cd044fbc38930b35ebcfbfe3e1bb6f0ff7
highwind.sg <highwind.sg@gmail.com>
2018-06-21 07:04:58 +0800

    learning python

---
+++ ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Caven Yip'
+my_age = 45 # not a line
+my_height = 168 # cm
+my_weight = 95 # kg
+my_eyes = 'black'
+my_teeth = 'white'
+my_hair = 'black'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} cm tall.")
+print(f"He's {my_weight} kg heavy.")
+print("Actually that's quite a bit heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+# this line is tricky, try to get it exactly right
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
