31169c036c2c365a17348c1717f5a6e7a51a899e
highwind.sg <highwind.sg@gmail.com>
2018-06-22 06:50:56 +0800

    adding new ex 7 & 8
    adding new ex 7 & 8

--- ex7.py
+++ ex7.py
@@ -1,6 +1,8 @@
+print("*" * 10)
 print("Mary had a little lamb.")
 print("Its fleece was white as {}.".format('snow'))
 print("And everywhere that Mary went.")
+print("*" * 10)
 print("." * 10)  # what'd that do?
 
 end1 = "C"
@@ -19,3 +21,4 @@ end12 = "r"
 # watch end = ' ' at the end. try removing it to see what happens
 print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
 print(end7 + end8 + end9 + end10 + end11 + end12)
+print('.' * 10)
