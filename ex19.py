0e4882553255489b0709b79d6eb40a0439624ef5
highwind.sg <highwind.sg@gmail.com>
2018-06-24 18:59:30 +0800

    added new line
    added new line

--- ex19.py
+++ ex19.py
@@ -1,6 +1,6 @@
 def cheese_and_crackers(cheese_count, boxes_of_crackers):
-	print(f"You have {cheese_count} cheeses!")
-	print(f"You have {boxes_of_crackers} boxes of crackers!")
+	print(f"You now have {cheese_count} cheeses!")
+	print(f"And also you have {boxes_of_crackers} boxes of crackers!")
 	print("Man that's enough for a party!")
 	print("Get a blanket.\n")
 
@@ -12,7 +12,6 @@ cheese_and_crackers(20, 30)
 print("OR, we can use variables from our script:")
 amount_of_cheese = 10
 amount_of_crackers = 50
-
 cheese_and_crackers(amount_of_cheese, amount_of_crackers)
 
 
@@ -22,3 +21,9 @@ cheese_and_crackers(10 + 20, 5 + 6)
 
 print("And we can combine the two, variables and math:")
 cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
+
+
+print("Still we can ask how many cheese and crackers you want.")
+num_cheese = input("How may cheese do you want?: ")
+num_boxes = input("How many boxes of crackers do you want?: ")
+cheese_and_crackers(num_cheese, num_boxes)
