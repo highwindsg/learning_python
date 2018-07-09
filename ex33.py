7f5555ad6da0c05510864ec70d9b54cb1e8ccb67
highwind.sg <highwind.sg@gmail.com>
2018-07-10 07:04:28 +0800

    new files
    new files

--- ex33.py
+++ ex33.py
@@ -2,12 +2,12 @@ i = 0
 numbers = []
 
 while i < 6:
-    print(f"At the top i is {i}")
+    print(f"Now, at the top i is {i}")
     numbers.append(i)
-    print("Numbers now: ", numbers)
+    print("So numbers in the list is now: ", numbers)
 
     i = i + 1
-    print(f"At the bottom i is {i}")
+    print(f"And now, at the bottom i is {i}")
     print("\n")
 
 print("The numbers: ")
