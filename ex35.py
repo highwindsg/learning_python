316f5ba712d8d70ec487891c0891df609a676db9
highwind.sg <highwind.sg@gmail.com>
2018-07-12 06:27:13 +0800

    new edit
    new edit

--- ex35.py
+++ ex35.py
@@ -7,20 +7,20 @@ def gold_room():
     if "0" in choice or "1" in choice:
         how_much = int(choice)
     else:
-        dead("Man, learn to type a number.")
+        dead("Oh man..., so little??")
 
     if how_much < 50:
         print("Nice, you're not greedy, you win!")
         exit(0)
     else:
-        dead("You greedy bastard!")
+        dead("You greedy fellow!")
 
 
 def bear_room():
-    print("There is a bear here.")
+    print("There is a fat bear here.")
     print("The bear has a bunch of honey.")
-    print("The fat bear is in front of another door.")
-    print("How are you going to move the bear?")
+    print("However the bear is in front of another door.")
+    print("How are you going to move the bear? ('take honey' or 'taunt bear')")
     bear_moved = False
 
     while True:
@@ -29,8 +29,9 @@ def bear_room():
         if choice == "take honey":
             dead("The bear looks at you then slaps your face off.")
         elif choice == "taunt bear" and not bear_moved:
-            print("The bear has moved from the door.")
+            print("The bear has moved away from the door.")
             print("You can go through it now.")
+            print("So what do you decide to do next? ('take honey', 'taunt bear' or 'open door')")
             bear_moved = True
         elif choice == "taunt bear" and bear_moved:
             dead("The bear gets pissed off and chews your leg off.")
@@ -40,36 +41,36 @@ def bear_room():
             print("I got no idea what that means.")
 
 
-def cthulhu_room():
-    print("Here you see the great evil Cthulhu.")
-    print("He, it, whatever stares at you and you go insane.")
-    print("Do you flee for your life or eat your head?")
+def bigrock_room():
+    print("And you see a great big rock rolling towards you!")
+    print("You screamed out loud and almost don't know what to do.")
+    print("Do you run for your life or stay put and continue to scream your head off? ('run' or 'stay')")
 
     choice = input("> ")
 
-    if "flee" in choice:
+    if "run" in choice:
         start()
-    elif "head" in choice:
-        dead("Well that was tasty!")
+    elif "stay" in choice:
+        dead("The big rock nearly rolls over you.")
     else:
-        cthulhu_room()
+        bigrock_room()
 
     
 def dead(why):
-    print(why, "Good job!")
+    print(why, "You survived and left. Good job!")
     exit(0)
 
 def start():
     print("You are in a dark room.")
     print("There is a door to your right and left.")
-    print("Which one do you take?")
+    print("Which one do you take? (left or right)")
 
     choice = input("> ")
 
     if choice == "left":
         bear_room()
     elif choice == "right":
-        cthulhu_room()
+        bigrock_room()
     else:
         dead("You stumble around the room until you starve.")
 
