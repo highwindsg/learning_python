12d4226a214f73feea70c392025baa40af354650
highwind.sg <highwind.sg@gmail.com>
2018-07-14 15:55:59 +0800

    practise old ex
    practise old ex

---
+++ ex7a.py
@@ -0,0 +1,23 @@
+print("*" * 10)
+print("I walk into the supermart and what did I see?")
+print("All items on {}% discount this week!".format(30))
+print("*" * 10)
+print("." * 10)
+
+end1 = "H"
+end2 = "A"
+end3 = "P"
+end4 = "P"
+end5 = "Y"
+end6 = "4th "
+end7 = "o"
+end8 = "f"
+end9 = "J"
+end10 = "U"
+end11 = "L"
+end13 = "Y"
+
+print(end1 + end2 + end3 + end4 + end5, end=' ')
+print(end6 + end7 + end8, end=' ')
+print(end9 + end10 + end11 + end13)
+print("." * 10)
---
+++ ex8a.py
@@ -0,0 +1,12 @@
+formatter = "{} {} {} {}"
+
+print(formatter.format("d", "r", "m", "f"))
+print(formatter.format("Do", "Re", "Me", "Fa"))
+print(formatter.format("The", "sound", "of", "music"))
+print(formatter.format(formatter, formatter, formatter, formatter))
+print(formatter.format(
+    "There was a time",
+    "when people say",
+    "Singapore wouldn't make it",
+    "but we did."
+))
