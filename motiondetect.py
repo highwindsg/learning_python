47f6f611c490aeb11735313c0e64bb454f92614d
highwind.sg <highwind.sg@gmail.com>
2018-07-22 19:55:46 +0800

    new files
    new files

--- motiondetect.py
+++ motiondetect.py
@@ -1,10 +1,10 @@
-import cv2                            # importing Python OpenCV
 from datetime import datetime         # importing datetime for naming files w/ timestamp
+import cv2                            # importing Python OpenCV
 
 def diffImg(t0, t1, t2):              # Function to calculate difference between images.
-  d1 = cv2.absdiff(t2, t1)
-  d2 = cv2.absdiff(t1, t0)
-  return cv2.bitwise_and(d1, d2)
+    d1 = cv2.absdiff(t2, t1)
+    d2 = cv2.absdiff(t1, t0)
+    return cv2.bitwise_and(d1, d2)
 
 threshold = 81500                     # Threshold for triggering "motion detection"
 cam = cv2.VideoCapture(0)             # Lets initialize capture on webcam
@@ -20,17 +20,17 @@ t_plus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
 timeCheck = datetime.now().strftime('%Ss')
 
 while True:
-  cv2.imshow( winName, cam.read()[1] )		# comment to hide window
-  if cv2.countNonZero(diffImg(t_minus, t, t_plus)) > threshold and timeCheck != datetime.now().strftime('%Ss'):
-    dimg= cam.read()[1]
+    cv2.imshow( winName, cam.read()[1] )		# comment to hide window
+    if cv2.countNonZero(diffImg(t_minus, t, t_plus)) > threshold and timeCheck != datetime.now().strftime('%Ss'):
+        dimg= cam.read()[1]
     cv2.imwrite(datetime.now().strftime('%Y%m%d_%Hh%Mm%Ss%f') + '.jpg', dimg)
-  timeCheck = datetime.now().strftime('%Ss')
-  # Read next image
-  t_minus = t
-  t = t_plus
-  t_plus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
+    timeCheck = datetime.now().strftime('%Ss')
+    # Read next image
+    t_minus = t
+    t = t_plus
+    t_plus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
 
-  key = cv2.waitKey(10)
-  if key == 27:
-    cv2.destroyWindow(winName)			# comment to hide window
+    key = cv2.waitKey(10)
+    if key == 27:
+        cv2.destroyWindow(winName)			# comment to hide window
     break
