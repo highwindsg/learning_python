4a7e6b8fdd21cf64cffda66efcead36b6c2b3ab8
highwind.sg <highwind.sg@gmail.com>
2018-07-22 21:07:30 +0800

    bracket changes
    bracket changes

--- motiondetect.py
+++ motiondetect.py
@@ -1,13 +1,14 @@
-from datetime import datetime         # importing datetime for naming files w/ timestamp
-import cv2                            # importing Python OpenCV
+from datetime import datetime           # importing datetime for naming files w/ timestamp
+import cv2                              # importing Python OpenCV
 
-def diffImg(t0, t1, t2):              # Function to calculate difference between images.
+def diffImg(t0, t1, t2):                # Function to calculate difference between images.
     d1 = cv2.absdiff(t2, t1)
     d2 = cv2.absdiff(t1, t0)
     return cv2.bitwise_and(d1, d2)
 
-threshold = 10000                     # Threshold for triggering "motion detection"
-cam = cv2.VideoCapture(0)             # Lets initialize capture on webcam
+# threshold - how much a pixel has to change by to be marked as "changed".
+threshold = 10000                       # Threshold for triggering "motion detection"
+cam = cv2.VideoCapture(0)               # Lets initialize capture on webcam
 
 winName = "Movement Indicator"	        # comment to hide window, if not necessary to run
 cv2.namedWindow(winName)		# comment to hide window, if not necessary to run
@@ -20,7 +21,7 @@ t_plus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
 timeCheck = datetime.now().strftime('%Ss')
 
 while True:
-    cv2.imshow( winName, cam.read()[1] )		# comment to hide window
+    cv2.imshow(winName, cam.read()[1])    # comment to hide window
     if cv2.countNonZero(diffImg(t_minus, t, t_plus)) > threshold and timeCheck != datetime.now().strftime('%Ss'):
         dimg = cam.read()[1]
         cv2.imwrite(datetime.now().strftime('%Y%m%d_%Hh%Mm%Ss%f') + '.jpg', dimg)
@@ -32,5 +33,5 @@ while True:
 
     key = cv2.waitKey(10)
     if key == 27:
-        cv2.destroyWindow(winName)			# comment to hide window
+        cv2.destroyWindow(winName)	    # comment to hide window
         break
