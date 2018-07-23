84cc2c8ef89042fc8544d96baab728bfbfec34f7
highwind.sg <highwind.sg@gmail.com>
2018-07-23 21:22:14 +0800

    update threshold
    threshold = 701500

--- motiondetect.py
+++ motiondetect.py
@@ -7,7 +7,7 @@ def diffImg(t0, t1, t2):                # Function to calculate difference betwe
     return cv2.bitwise_and(d1, d2)
 
 # threshold - how much a pixel has to change by to be marked as "changed".
-threshold = 10000                       # Threshold for triggering "motion detection"
+threshold = 701500                      # Threshold for triggering detection. eg. 701500(iMac),81500(default)
 cam = cv2.VideoCapture(0)               # Lets initialize capture on webcam
 
 winName = "Movement Indicator"	        # comment to hide window, if not necessary to run
@@ -23,6 +23,7 @@ timeCheck = datetime.now().strftime('%Ss')
 while True:
     cv2.imshow(winName, cam.read()[1])    # comment to hide window
     if cv2.countNonZero(diffImg(t_minus, t, t_plus)) > threshold and timeCheck != datetime.now().strftime('%Ss'):
+      # dimg=diffImg(t_minus, t, t_plus)
         dimg = cam.read()[1]
         cv2.imwrite(datetime.now().strftime('%Y%m%d_%Hh%Mm%Ss%f') + '.jpg', dimg)
     timeCheck = datetime.now().strftime('%Ss')
