1f0c6fa7ed6133c6023c868418a38eca17fd2c4f
highwind.sg <highwind.sg@gmail.com>
2018-08-02 06:09:47 +0800

    new files
    new files

--- ex38.py
+++ ex38.py
@@ -1,6 +1,6 @@
 ten_things = "Apples Oranges Crows Telephone Light Sugar"
-
-print("Wait there are not 10 things in that list. Let's fix that.")
+print(f"The 10 things are {ten_things}")
+print("Wait ... there are not 10 things in that list. Let's fix that.")
 
 stuff = ten_things.split(' ')
 more_stuff = ["Day", "Night", "Song", "Frisbee",
---
+++ ex39.py
@@ -0,0 +1,31 @@
+# below is example of a list
+things = ['a', 'b', 'c', 'd']
+print(things[1])
+
+things[1] = 'z'
+print(things[1])
+
+things
+print("\n")
+
+# below is example of a dictionary
+stuff = {'name': 'Zed', 'age': 39, 'height': 6 * 12 + 2}
+print(stuff['name'])
+
+print(stuff['age'])
+
+print(stuff['height'])
+
+stuff['city'] = "SF"
+print(stuff['city'])
+print("\n")
+
+# below is example of putting new things
+# into dictionary with strings
+stuff[1] = "Wow"
+stuff[2] = "Neato"
+print(stuff[1])
+
+print(stuff[2])
+print("\n")
+
