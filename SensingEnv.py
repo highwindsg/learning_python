0877748a694808a6d2172fbe0789c87e91079db4
highwind.sg <highwind.sg@gmail.com>
2018-08-16 06:59:58 +0800

    new file
    new file

--- SensingEnv.py
+++ SensingEnv.py
@@ -8,7 +8,7 @@ while True:
     temp = round(sense.get_temperature()*1.8 +32)
     humidity = round(sense.get_humidity())
     pressure = round(sense.get_pressure())
-    message = ' T=%dF, H=%d ' %(temp,humidity,pressure)
+    message = ' T=%dF, H=%d, P=%d ' %(temp, humidity, pressure)
     sense.show_message(message, scroll_speed = (0.08), text_colour = [200,240,200], back_colour = [0,0,0,0])
     time.sleep(4)
     log = open('weather.txt', "a")
