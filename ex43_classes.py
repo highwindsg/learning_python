8899c9cd89c991432db5aa6ca6cce7b42f3bd74c
Caven Yip <highwind.sg@gmail.com>
2018-08-17 09:20:53 +0800

    new file
    new file

--- ex43_classes.py
+++ ex43_classes.py
@@ -9,47 +9,47 @@ class Engine(object):   # Make a class named Engine that is-a object.
     def __init__(self, scene_map):  # The init function that takes self and scene_map params.
         pass
 
-    def play(self):
+    def play(self):     # Define a play function with a params self.
         pass
 
-class Death(Scene):
+class Death(Scene):     # Make a class named Death that is-a Scene.
 
-    def enter(self):
+    def enter(self):    # Define a enter function with a params self.
         pass
 
-class CentralCorridor(Scene):
+class CentralCorridor(Scene):   # Make a class named CentralCorridor that is-a Scene.
 
-    def enter(self):
+    def enter(self):    # Define a enter function with params self.
         pass
 
-class LaserWeaponArmory(Scene):
+class LaserWeaponArmory(Scene):     # Make a class named LaserWeaponArmory that is-a Scene.
 
-    def enter(self):
+    def enter(self):    # Define a enter function with params self.
         pass
 
-class TheBridge(Scene):
+class TheBridge(Scene):     # Make a class named TheBridge that is-a Scene.
 
-    def enter(self):
+    def enter(self):    # Define a enter function with params self.
         pass
 
-class EscapePod(Scene):
+class EscapePod(Scene):     # Make a class named EscapePod that is-a Scene.
 
-    def enter(self):
-        pass
-
-
-class Map(object):
-
-    def __init__(self, start_scene):
-        pass
-
-    def nect_scene(self, scene_name):
-        pass
-
-    def opening_scene(self):
+    def enter(self):    # Define a enter function with params self.
         pass
 
 
-a_map = Map('central_corridor')
-a_game = Engine(a_map)
-a_game.play()
+class Map(object):      # Make a class named Map that is-a object.
+
+    def __init__(self, start_scene):    # The function has a init that takes self and start_scene params.
+        pass
+
+    def next_scene(self, scene_name):   # The function named next_scene takes self and scene_name params.
+        pass
+
+    def opening_scene(self):    # The function named opening_scene takes the self params.
+        pass
+
+
+a_map = Map('central_corridor')     # Set a_map to an instance of class Map that takes params self and 'central_corridor'.
+a_game = Engine(a_map)      # Set a_game to an instance of class Engine that takes params self and a_map.
+a_game.play()       # From a_game get the play function.
