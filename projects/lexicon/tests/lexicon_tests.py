ae43905a0a67e263de19582626b9cb172cd1c84c
highwind.sg <highwind.sg@gmail.com>
2018-09-02 06:46:21 +0800

    new files
    new files

--- projects/lexicon/tests/lexicon_tests.py
+++ projects/lexicon/tests/lexicon_tests.py
@@ -1,8 +1,8 @@
 from nose.tools import *
-import lexicon 
+import lexicon
 
 def test_directions():
-    assert_equal(lexicon.scan("north"), [('direction', 'north')])
+    assert_equal(lexicon[('direction', 'north')], [('direction', 'south')], [('direction', 'east')])
     result = lexicon.scan("north south east")
     assert_equal(result, [('direction', 'north'),
                           ('direction', 'south'),
