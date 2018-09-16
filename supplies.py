3c51e79fac87b61bd5523f43ebf23e631217a0e9
highwind.sg <highwind.sg@gmail.com>
2018-09-16 09:29:43 +0800

    new files
    new files

---
+++ supplies.py
@@ -0,0 +1,6 @@
+supplies = ["pens", "staplers", "flames-throwers", "binders"]
+for i in range(len(supplies)):
+    print("Index " + str(i) + " in supplies is: " + supplies[i])
+
+# len(supplies) is calculating the number or items in the supplies list, which is 4.
+# Therefore is read as range(4) for the range() function in line 2.
