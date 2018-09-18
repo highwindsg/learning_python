cc26f388fc18062005da6692e37360cf93bbc379
Caven Yip <highwind.sg@gmail.com>
2018-09-18 12:50:39 +0800

    new files
    new files

--- magic8Ball2.py
+++ magic8Ball2.py
@@ -8,6 +8,9 @@ messages = ["It is certain",
     "Conentrate and ask again",
     "My reply is no",
     "Outlook not so good",
-    "very doubtful"]
+    "Very doubtful"]
 
+# Therefore print a random line of message from the list above.
+# len(messages) shows there are 9 lines.
+# random.randint() will choose a random line number.
 print(messages[random.randint(0, len(messages) - 1)])
