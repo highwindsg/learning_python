64b56ad6df8def0cf72e413b2a2e78332433cc46
highwind.sg <highwind.sg@gmail.com>
2018-09-22 16:04:58 +0800

    new files
    new files

---
+++ hc-sr501-cam-test.py
@@ -0,0 +1,13 @@
+# https://dronebotworkshop.com/using-pir-sensors-with-arduino-raspberry-pi/
+
+from gpiozero import MotionSensor
+from picamera import PiCamera
+
+pir = MotionSensor(4)
+camera = PiCamera()
+
+while True:
+    pir.wait_for_motion()
+    if pir.motion_detected:
+        print("Good Looking Person Detected")
+        camera.start_preview()
