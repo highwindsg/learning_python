4dc57b89b34b231904aff32f7048604b11301d3c
highwind.sg <highwind.sg@gmail.com>
2018-10-04 06:26:44 +0800

    new file
    new file

--- search1.py
+++ search1.py
@@ -4,6 +4,8 @@ for line in fhand:
     if line.startswith('From:'):
         print(line)
 
+# Use the '.startswith()' string method to select only those prefix we want.
+
 # The script will produce extra blank lines in the output.
 # This is due to the invisible newline character.
 # Each of the lines ends with a newline, so the print
