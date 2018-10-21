ce7b1e78c277c1cc4258062a0ed3526b4ad3cf26
Caven Yip <highwind.sg@gmail.com>
2018-10-21 18:29:10 +0800

    new file
    new file

---
+++ chat_dict.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+chat_dict = {"happy": "I;m happy too!",
+             "sad": "Tell me al about it.",
+             "raspberry": "Oh yum! I love raspberries!",
+             "computer": "Computers will over the world! You're already taling to one",
+             "music": "Have you heard the latest Depeche Mode album?",
+             "art": "But what is art really, anyway?",
+             "joke": "I only know this joke: How do you kill a circus? Go for the juggler.",
+             "python": "I hate snakes!",
+             "stupid": "Who are you calling stupid, jelly brain?",
+             "weather": "I wonder if the sun will shine on Saturday?",
+             "you": "Leave me out of this!",
+             "certain": "How can you be so confident?",
+             "talk": "You're all talking only! Do something!",
+             "think": "You can overthink these things, though.",
+             "hello": "Why, hello to you too, buddy!",
+             "wearing": "I don't wear clothes. I don't even come with a case."}
