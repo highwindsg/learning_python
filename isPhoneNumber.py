7e18be196593adf68d23a0fdcf6bc2f2c9d92133
Caven Yip <highwind.sg@gmail.com>
2018-10-22 20:23:47 +0800

    new file
    new file

--- isPhoneNumber.py
+++ isPhoneNumber.py
@@ -7,7 +7,7 @@ def isPhoneNumber(text):
     for i in range(0, 3):   # Check the area code only contains characters.
         if not text[i].isdecimal():
             return False
-    if text[3] != "-":      # Check must have first hyphen after the area code. 
+    if text[3] != "-":      # Check must have first hyphen after the area code.
         return False
     for i in range(4, 7):   # Check next for 3 more characters.
         if not text[i].isdecimal():
@@ -27,8 +27,22 @@ def isPhoneNumber(text):
 
 message = "Call me at 415-555-1011 tomorrow. 415-555-9999 is my office."
 
+# On each iteration of the 'for loop', a new chunk of 12 characters from
+# 'message' is assigned to the variable 'chuck'.
+# For eg, on the first iteration, i is 0, and 'chunk' is assigned [0:12],
+# which is the string ('Call me at 4').
+# And on the next iteration, i is 1, and 'chunk' is assigned message [1:13],
+# which is the string ('all me at 41').
 for i in range(len(message)):
     chunk = message[i:i+12]
+
+# You pass 'chunk' to 'isPhoneNumber()' to see whether it matches the phone no.
+# pattern, and if so you print the chunk.
     if isPhoneNumber(chunk):
         print("Phone number found: " + chunk)
+
+# The 'for loop' continue to loop through the message, testing each 12-character
+# piece and printing any 'chunk' it finds that satisfies 'isPhoneNumber()'.
+# Once done going through the 'message', we print 'Done'.
 print("Done")
+
