b74d0df072fd8edcf35bbbb497fb569a7ddb39ab
highwind.sg <highwind.sg@gmail.com>
2018-11-07 06:31:13 +0800

    new file
    new file

--- renameDates.py
+++ renameDates.py
@@ -9,23 +9,32 @@ datePattern = re.compile(r"""^(.*?) # all text before the date
     ((0|1)?\d)-                     # one or two digits for the month
     ((0|1|2|3)?\d)-                 # one or two digits for the day
     ((19|20)\d\d)                   # four digits for the year
-    (.*?)$
+    (.*?)$                          # match any text that comes after the date
     """, re.VERBOSE)
 
 # TODO: Loop over the files in the working directory.
+for amerFilename in os.listdir("."):
+    mo = datePattern.search(amerFilename)
 
+    # TODO: Skip files without a date.
+    if mo == None:
+        continue
 
-# TODO: Skip files without a date.
+    # TODO: Got the different parts of the filename.
+    beforePart = mo.group(1)
+    monthPart  = mo.group(2)
+    dayPart    = mo.group(4)
+    yearPart   = mo.group(6)
+    afterPart  = mo.group(8)
 
+    # TODO: Form the European-style filename.
+    euroFilename = beforePart + dayPart + "-" + monthPart + "-" + yearPart + afterPart
 
-# TODO: Got the different parts of the filename.
+    # TODO: Get the full, absolute file paths.
+    absWorkingDir = os.path.abspath(".")
+    amerFilename = os.path.join(absWorkingDir, amerFilename)
+    euroFilename = os.path.join(absWorkingDir, euroFilename)
 
-
-# TODO: Form the European-style filename.
-
-
-# TODO: Get the full, absolute file paths.
-
-
-# TODO: Rename the files.
-
+    # TODO: Rename the files.
+    print('Renaming "%s" to "%s"...' % (amerFilename, euroFilename))
+    #shutil.move(amerFilename, euroFilename)    # uncomment after testing
