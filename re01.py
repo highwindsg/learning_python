04eb9018cef3e703c229b508664628f507b0306c
highwind.sg <highwind.sg@gmail.com>
2018-12-03 07:10:33 +0800

    new files
    new files

--- re01.py
+++ re01.py
@@ -6,5 +6,5 @@ import re
 hand = open("mbox-short.txt")
 for line in hand:
     line = line.rstrip()
-    if re.search("From", line):
-        print(line)
+    if re.search("From", line):     # From 're' module, use the 'search' method to find
+        print(line)                 # lines that starts with 'From' and print then out.
