aab001271830512a40b4481f8b684c89f9fdfa3c
highwind.sg <highwind.sg@gmail.com>
2019-01-09 07:08:46 +0800

    new files
    new files

---
+++ usersAnswer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+usersAnswer = input("What is 6 + 3? ")  # Get the user's answer
+usersAnswer = int(usersAnswer)  # converts to an integer
+if usersAnswer == 9:
+    print("Yes you are correct")
+    print("You are a genius")
+else:
+    print("Sorry, that is not correct")
+    print("The correct answer was 9")
---
+++ yes_or_no.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+answer = input("Are you ready (yes or no)? ")
+if answer == "yes":
+    print("OK, here we go.")
+
