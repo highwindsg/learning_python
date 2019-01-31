ab8d2456071b5ebf2d607985c779e1ff4de1a829
highwind.sg <highwind.sg@gmail.com>
2019-01-31 08:15:51 +0800

    new file
    new file

---
+++ even_odd.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def even_odd():
+    x = int(input("Type a number: "))
+    if x % 2 == 0:
+        print("even")
+    else:
+        print("odd")
+
+even_odd()
+even_odd()
+even_odd()
