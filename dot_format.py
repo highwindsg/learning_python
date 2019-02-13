b46371d55d912ccc0fbd77c4566d63c17e6fb52d
highwindsg <highwind.sg@gmail.com>
2019-02-14 07:01:49 +0800

    new file

---
+++ dot_format.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+ans1 = input("What did you write yesterday?")
+ans2 = input("Where did you go yesterday?")
+string1 = "Yesterday I wrote a {}. I sent it to {}.".format(ans1, ans2)
+print(string1)
