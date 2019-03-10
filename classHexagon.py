8dff1ef8ac3b8e3341819632b309092111ec8b87
highwindsg <highwind.sg@gmail.com>
2019-03-10 10:43:26 +0800

    new file

---
+++ classHexagon.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Create a Hexagon class with a method called calculate_perimeter that
+# calculates and returns its perimeter.
+# Then create a Hexagon object, and call the calculate_perimeter method
+# on it, and print the result.
+class Hexagon():
+    def __init__(self, s1, s2, s3, s4, s5, s6):
+        self.s1 = side1
+        self.s2 = side2
+        self.s3 = side3
+        self.s4 = side4
+        self.s5 = side5
+        self.s6 = side6
+
+    def calculate_perimeter(self):
+        HexPerimeter = side1 + side2 + side3 + side4 + side5 + side6
+        return HexPerimeter
+
