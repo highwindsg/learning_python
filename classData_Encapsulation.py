6adb4a0f9e71356b69b9283b630ca58ef0470aa4
highwindsg <highwind.sg@gmail.com>
2019-03-10 19:20:43 +0800

    new file

--- classData_Encapsulation.py
+++ classData_Encapsulation.py
@@ -13,6 +13,25 @@ class Data():  # Create a class named Data()'.
         self.nums[index] = n    # From self, get the instance var of 'nums' with indexing and
                                 # assign to 'n'.
 
-""" The class Data has an instance var called 'nums' that contains a list of integers.
+""" The class Data has an instance variable called 'nums' that contains a list of integers.
 Once you create a Data object, there are two ways you can change the items in 'nums'.
+"""
+
+""" You can change the items in 'nums' by directly accessing the 'nums' instance variable:,
+"""
+data_one = Data()       # Assign var 'data_one' to class Data().
+data_one.nums[0] = 100  # From var 'data_one', get the instance variable of 'nums' list at the
+                        # first index and assign to 100.
+print(data_one.nums)
+
+"""
+or by using the 'change_data' method:
+"""
+data_two = Data()       # Assign var 'data_two' to class Data().
+data_two.change_data(0, 100)    # From var 'data_two', get the instance variable of
+                                # 'change_data' and with params 0 and 100.
+print(data_two.nums)
+
+""" Note that if the above 'nums' list is changed to a tuple instead of a list, then the
+code will not work because tuples are immutable , means cannot be changed.
 """
