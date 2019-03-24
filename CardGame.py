addc46a24f944cf7d014084840cad918673ca647
highwind.sg <highwind.sg@gmail.com>
2019-03-25 06:46:34 +0800

    new files
    new files

--- CardGame.py
+++ CardGame.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
+from random import shuffle
+
 class Card:
 
-    # suits and values are class variables.
+    # suits and values lists are class variables.
 
     """
     suits are arranged in order of strength--with the strongest
@@ -49,7 +51,7 @@ class Card:
     occurs, the methods use the index value of the suits to break
     the tie.
     """
-    def __lt__(self, c2):
+    def __lt__(self, c2):   # c2 param represent comparing card two.
         if self.value < c2.value:
             return True
         if self.value == c2.value:
@@ -57,8 +59,8 @@ class Card:
                 return True
             else:
                 return False
-        return False
-
+        else:               # This else line is actually redundant.
+            return False
 
     def __gt__(self, c2):
         if self.value > c2.value:
@@ -68,7 +70,8 @@ class Card:
                 return True
             else:
                 return False
-        return False
+        else:               # This else line is actually redundant.
+            return False
 
     """
     The magic method __repr__ uses the value and suit instance var
@@ -80,19 +83,61 @@ class Card:
         v = self.values[self.value] + " of " + self.suits[self.suit]
         return v
 
+
+"""
+When you initialize the Deck obj, the two for-loops in __init__
+create Card objs representing all cards in a 52-card deck and
+appends them to the cards list. So if 52 divides by 4, we take
+the first loop from 2 to 15 because the first value for a card
+is 2, and the last value for a card is 14 (the ace).
+Each time around the inner loop, a new card is created using
+the int from outer loop as the value (i.e. 14 for an ace) and
+the int from the inner loop as the suit (i.e. a 2 for hearts).
+This process creates 52 cards--one card for every suit and value
+combinations. After the method creates the cards, the shuffle
+method from the random module rearranges the items in the cards
+list; mimicking the shuffling of a deck of cards.
+Our deck has one other method called rm_card that removes and
+returns a card from the cards list, or returns None if it is
+empty.
+"""
+class Deck:
+    def __init__(self):
+        self.cards = []
+        for i in range(2, 15):  # up to 15 but not including 15.
+            for j in range(4):
+                self.cards.append(Card(i, j))
+        shuffle(self.cards)
+
+    def rm_card(self):
+        if len(self.cards) == 0:
+            return None
+        else:
+            return self.cards.pop()
+
+
 # Client call to test out the lesser than function method.
 card1 = Card(10, 2)
 card2 = Card(11, 3)
 print(card1 < card2)
+print("")
 
 # Client call to test out the greater than function method.
 card1 = Card(10, 2)
 card2 = Card(11, 3)
 print(card1 > card2)
+print("")
 
 # Client call to test out the representation function method
 # to print out what the value and suit represents in the values and
 # suits lists.
 card = Card(10, 2)
 print(card)
+print("")
+
+# You can use the Deck class to create a new deck of cards and
+# print each card in it.
+deck = Deck()
+for card in deck.cards:
+    print(card)
 
