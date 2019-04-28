782a0bda9f1b3571c5336391332929230398467e
highwindsg <highwind.sg@gmail.com>
2019-04-28 18:05:01 +0800

    new files

---
+++ turtle4.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from turtle import *
+
+color("red")
+
+for i in range(5):
+    forward(200)
+    left(216)
+
+bye()
+
---
+++ turtle5.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from turtle import *
+
+color("red")
+
+# Below for loop draws a 20-point star.
+for i in range(20):
+    forward(200)
+    left(162)
+
+bye()
+
