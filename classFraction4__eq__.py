b62ac6458e79c68b461dace5e70a35c996296269
highwind.sg <highwind.sg@gmail.com>
2019-04-30 06:49:29 +0800

    new files
    new files

---
+++ classFraction4__eq__.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+class Fraction:
+    def __init__(self, top, bottom):
+        self.num = top
+        self.den = bottom
+
+    def __str__(self):
+        return str(self.num)+"/"+str(self.den)
+
+    def show(self):
+        print(self.num,"/",self.den)
+
+    def __add__(self, otherfraction):
+        newnum = self.num * otherfraction.den + \
+                    self.den * otherfraction.num
+        newden = self.den * otherfraction.den
+#        common = self.gcd(newnum, newden)
+        return Fraction(newnum//common, newden//common)
+
+    def __eq__(self, other):
+        firstnum = self.num * other.den
+        secondnum = other.num * self.den
+
+        return firstnum == secondnum
+
+f1 = Fraction(1, 4)
+f2 = Fraction(1, 2)
+f3 = f1 + f2
+print(f3)
+
