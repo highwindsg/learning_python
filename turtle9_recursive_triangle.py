20754d53babf5560b38f21755c6953246ccde988
highwind.sg <highwind.sg@gmail.com>
2019-05-12 07:20:34 +0800

    new files
    new files

--- turtle9_recursive_triangle.py
+++ turtle9_recursive_triangle.py
@@ -6,12 +6,22 @@ from turtle import *
 t = Turtle()
 myWin = t.getscreen()
 
+color("red")
+
 def triangle(lineLen, t):
     if lineLen > 5:
         t.forward(lineLen)
         t.left(120)
         triangle(lineLen +5, t)
-        t.left(120)
         t.forward(lineLen)
         t.left(120)
+        triangle(lineLen +5, t)
+        t.forward(lineLen)
+        t.left(120)
+        triangle(lineLen +5, t)
+        t.forward(lineLen)
+
+
+triangle(100, t)
+myWin.exitonclick()
 
