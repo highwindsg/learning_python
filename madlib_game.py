6716c44109472ed981371e9be460acef118ace6c
highwind.sg <highwind.sg@gmail.com>
2019-05-27 19:17:40 +0800

    new files
    new files

---
+++ madlib_game.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+color = input("Enter a color: ")
+Plural_noun = input("Enter a plural noun: ")
+celebrity_name = input("Name of your favourite celebrity: ")
+
+print("Roses are " + color)
+print(Plural_noun + " are blue")
+print("And I love " + celebrity_name)
