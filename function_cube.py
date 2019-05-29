d33b5c6b181c833f739b3fe270c0f00109e26d1e
highwind.sg <highwind.sg@gmail.com>
2019-05-30 06:36:11 +0800

    new files
    new files

---
+++ function_cube.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def cube(num):
+     return num * num * num
+
+result = cube(4)
+print(result)
+
---
+++ function_maxnum.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def max_num(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+
+print(max_num(200, 300, 400))
+
