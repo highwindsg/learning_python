f0db6995304b774af5c4da5f4648b91c2b130a33
highwind.sg <highwind.sg@gmail.com>
2019-05-30 06:35:30 +0800

    new files
    new files

---
+++ dict_mth_converter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+monthConversions = {
+    "Jan" : "January",
+    "Feb" : "February",
+    "Mar" : "March",
+    "Apr" : "April",
+    "May" : "May",
+    "Jun" : "June",
+    "Jul" : "July",
+    "Aug" : "August",
+    "Sep" : "September",
+    "Oct" : "October",
+    "Nov" : "November",
+    "Dec" : "December",
+}
+
+print(monthConversions["Jul"])
+print(monthConversions.get("Luv", "Not a valid key"))
+
