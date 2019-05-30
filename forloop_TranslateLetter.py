650e4e00cd28420d71311d70c10352ba06db23e1
highwind.sg <highwind.sg@gmail.com>
2019-05-31 07:12:52 +0800

    new file
    new file

---
+++ forloop_TranslateLetter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# To translate any vowels to become something else eg. 'g'.
+
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                translation = translation + "G"
+            else:
+                translation = translation + "g"
+        else:
+            translation = translation + letter
+    return translation
+
+
+print(translate(input("Enter a phrase: ")))
