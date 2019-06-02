d9ccbcf0bb57838b6a3fa2c8ba4ac983bfb95a2e
highwind.sg <highwind.sg@gmail.com>
2019-06-02 13:24:01 +0800

    Update forloop_ExponentFunction.py

--- forloop_ExponentFunction.py
+++ forloop_ExponentFunction.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
+# Create function 'raise_to_power' with two params.
 def raise_to_power(base_num, power_num):
-    result = 1
+    result = 1  # Set var obj 'result' to 1
     for index in range(power_num):
         result = result * base_num
     return result
 
 
-print(raise_to_power(3, 4))
+# Client call the function of 'raise_to_power()' with params 3 and 4, and then
+# print out the result.
+print(raise_to_power(3, 4)) # Read as 3 to the power of 4, eg. 3^4
