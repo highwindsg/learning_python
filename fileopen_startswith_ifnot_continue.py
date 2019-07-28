6bd7b1879b0af3f4489f870466c6b4a43b565666
highwind.sg <highwind.sg@gmail.com>
2019-07-28 11:12:48 +0800

    new files
    new files

--- fileopen_startswith_ifnot_continue.py
+++ fileopen_startswith_ifnot_continue.py
@@ -5,5 +5,7 @@ fhand = open("mbox-short.txt")
 # Use the .startswith() method to only print out lines that starts with 'From:'.
 for line in fhand:
     line = line.rstrip()        # Strip away the empty white spaces on the right side.
-    if line.startswith("From:"):
-        print(line)
+    if not "@uct.ac.za" in line:    # This means that if the quoted string is not found in line,
+        continue                    # then continue next line to find the quoted string.
+    print(line)
+
