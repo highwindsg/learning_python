0e6d0371956c8e31876a68e515b3f5a602ca6de1
highwind.sg <highwind.sg@gmail.com>
2019-08-10 15:03:11 +0800

    Update mysocket.py
    update

--- socket/mysocket.py
+++ socket/mysocket.py
@@ -2,5 +2,9 @@
 
 import socket
 
+# From socket module, use the .socket() method and parse in the two params of
+# Internet and streaming, and assign it to var 'mysock'.
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Use the .connect() method from 'mysock' var and connect to URL at tcp/80.
 mysock.connect( ("data.pr4e.org", 80) )
