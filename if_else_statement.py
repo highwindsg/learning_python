3912f57c0857a903bc2f17ae9799f36b15449670
highwind.sg <highwind.sg@gmail.com>
2019-08-22 07:25:13 +0800

    Create if_else_statement.py
    new files

---
+++ if_else_statement.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Change the value assignment of var 'x' from positive to negative number to see the different output.
+
+x = -100
+
+if x != 100 or x > 0:
+    print("x is = ", x)
+    print(x)
+if x > 0:
+    print("x is positive")
+else:
+    print("x is negative")
+
+print("finish")
