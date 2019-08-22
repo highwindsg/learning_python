4a51017956552537096f198ee5856c48eced3b52
highwind.sg <highwind.sg@gmail.com>
2019-08-23 07:14:53 +0800

    Create if_else_nested_statement.py
    new files

---
+++ if_else_nested_statement.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+name = input("Enter a name: ")
+
+if name == "Max":
+    print("Name entered is: ", name)
+elif name == "Leo":
+    print("Name entered is: ", name)
+elif name == "Roy":
+    print("Name entered is: ", name)
+elif name == "Eli":
+    print("Name entered is: ", name)
+else:
+    print("Name entered is invalid.")
