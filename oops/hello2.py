ea69c5d37e488e136f1a96877e878ccdc3929e48
highwind.sg <highwind.sg@gmail.com>
2019-08-30 07:35:47 +0800

    new files
    new files

---
+++ oops/hello2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+class Hello:    # Create a class named 'Hello'.
+    def __init__(self, name):   # Define a init start func with params 'self' and 'name'.
+        self.a = 10             # From self, set the var '.a' to 10.
+        self._b = 20            # From self, set the var '._b' to 20.
+        self.__c = 30           # From self, set the private var '.__c' to 30.
+    def public_method(self):    # Define a public_method func with param self.
+        self.a              # From self, get the attrib of 'a'.
+        self.__c            # From self, get the private attrib of '.__c'.
+        print("public")     # Print the word 'public'.
+
+
+hello = Hello("name")       # Set var obj hello to the Hello() class with param str 'name'.
+print(hello.a)              # Get the value of attrib .a from the hello var obj and print it out.
+print(hello._b)             # Get the value of attrib ._b from the hello var obj and print it out.
+hello.public_method()       # Just print the param of .public_method() func from var obj hello.
+
+"""
+Therefore self.__c will not be printed in output as it is a private member var in the class.
+"""
--- oops/rectangle.py
+++ oops/rectangle.py
@@ -1,9 +1,33 @@
 #!/usr/bin/env python3
 
+"""
+To declare a private member var or private attrib, use a double underscore in front of the var name.
+eg. 'self.__varName', so '.__varName' is a private member var of param 'self'.
+This is to prevent the value of the private var to be wrongly re-assigned with another value later
+on in the same program.
+Therefore a private member var is private to the class and cannot be used or called outside of the class.
+A private member var can also be used in different functions that is within the same class.
+"""
+
 class Rectangle:        # Create a class named 'Rectangle()'.
-    def __init__(self, height, width):  # Define a init start func with param self, height and width).
-        self.height = height            # From self, get the attrib of height and set it to height.
-        self.width = width              # From self, get the attrib of width and set it to width.
+    def __init__(self, height, width):  # Define a init start func with params self, height and width).
+        self.__height = height      # From self, set the private attrib of .__height and assign to height.
+        self.__width = width        # From self, set the private attrib of .__width and assign to width.
+        
+    def set_height(self, height):   # Define a func named set_height with params self and height.
+        self.__height = height      # From self, set the private attrib of .__height and assign to height.
+    def get_height(self, height):   # Define a func named get_height with params self and height.
+        return self.__height        # Return the value of the private var of self.__height to the get_height() func.
+    
+    def set_width(self, width):     # Define a func named set_width with params self and width.
+        self.__width = width        # From self, set the private attrib of .__width and assign to width.
+    def get_width(self, width):     # Define a func named get_width with params self and width.
+        return self.__width         # Return the value of the private var of self.__width to the get_width() func.
+    
+    def area(self):                 # Define a func named area with param self.
+        return self.__height * self.__width     # Calculate the area of the rectangle by multiplying
+                                                # the private values of self.__height and self.__width,
+                                                # return the value to the area() func.
 
 #    pass                # Insert the pass statement here to denote that this is a empty class.
 
@@ -15,11 +39,7 @@ rect2 = Rectangle(50, 40)     # 'rect2' obj is-a 'Rectangle()' with params 50 an
 #
 # rect1.width = 40        # For 'rect1', set the 'width' attrib to 40.
 # rect2.width = 10        # For 'rect2', set the 'width' attrib to 10.
-"""
-Since the attrib of 'height' and 'width' are set in the init start func from param 'self' in line 4 to line 6,
-therefore it is not necessary to explicitly assign the '.height' and '.width' in line 13 to line 17 again.
-"""
 
 # Client call to calculate the area of 'rect1' and 'rect2' and print out.
-print("Area of 'rect1': ", (rect1.height * rect1.width))
-print("Area of 'rect2': ", (rect2.height * rect2.width))
+print("Area of 'rect1': ", rect1.area())
+print("Area of 'rect2': ", rect2.area())
