3cd734837cc3c4b86b2b97d8bec6958192600ed2
highwind.sg <highwind.sg@gmail.com>
2019-08-31 07:07:54 +0800

    Update hello2.py
    update

--- oops/hello2.py
+++ oops/hello2.py
@@ -6,14 +6,22 @@ class Hello:    # Create a class named 'Hello'.
         self._b = 20            # From self, set the var '._b' to 20.
         self.__c = 30           # From self, set the private var '.__c' to 30.
     def public_method(self):    # Define a public_method func with param self.
-        self.a              # From self, get the attrib of 'a'.
-        self.__c            # From self, get the private attrib of '.__c'.
-        print("public")     # Print the word 'public'.
+        print(self.a)           # From self, get the attrib of 'a'.
+        print(self.__c)         # From self, get the private attrib of '.__c'.
+        print("public")         # Print the word 'public'.
+        self.__private_method()     # So even if a private method is defined later in the next line,
+                                    # it can also be used anywhere within the same class,
+                                    # but not outside of the class.
+
+    def __private_method(self):     # Define a private method named '__private_method()' with param self.
+        print("private")            # Print the word 'private' in this private method.
 
 
 hello = Hello("name")       # Set var obj hello to the Hello() class with param str 'name'.
 print(hello.a)              # Get the value of attrib .a from the hello var obj and print it out.
+print("")
 print(hello._b)             # Get the value of attrib ._b from the hello var obj and print it out.
+print("")
 hello.public_method()       # Just print the param of .public_method() func from var obj hello.
 
 """
