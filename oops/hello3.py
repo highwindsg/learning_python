ea4b1d58fc39cd35920e9fa696382cc8de6c2622
highwind.sg <highwind.sg@gmail.com>
2019-08-31 15:51:06 +0800

    Update hello3.py
    update

--- oops/hello3.py
+++ oops/hello3.py
@@ -2,6 +2,11 @@
 
 # If the module is from another directory, must mentioned from where you import the module name.
 from dir1 import myfunctions
+# or a faster way to import from dir1 get the myfunction name.
+import dir1.myfunctions
 
 print(myfunctions.add(2, 3))
 print(myfunctions.multiply(10, 3))
+print("")
+print(dir1.myfunctions.add(2, 3))
+print(dir1.myfunctions.multiply(10, 3))
