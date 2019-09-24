738364a59f78ba0453587b388786f8634bc37321
highwind.sg <highwind.sg@gmail.com>
2019-09-25 07:23:13 +0800

    Update closures2.py
    update file

--- ClosuresAndFunc/closures2.py
+++ ClosuresAndFunc/closures2.py
@@ -26,3 +26,25 @@ This means var 'a' is storing the state of the inner function even if the outer 
 And that is the magic of the closures.
 A closures function is able to remember the values (eg. text) which are declared outside the function.
 """
+print("")
+
+"""
+Creating a closures func to calculate the power of a given base number.
+"""
+def nth_power(exponent):    # Define an outer func named 'nth_power()' with param 'exponent'.
+    def pow_of(base):       # Define an inner func named 'pow_of()' with param 'base'.
+        return pow(base, exponent)  # Use the built-in func of pow() to calculate with 'base' and
+                                    # 'exponent' value, and return the answer to the inner func 'pow_of()'.
+    return pow_of       # Then finally return the value of 'pow_of' without parenthesis to the outer func.
+
+square = nth_power(2)   # Passing the exponent value of '2' into the func 'nth_power()', and assigning to var 'square'.
+print(square(2))        # Print the answer of square 2 to the power of 2.
+print(square(3))        # Print the answer of square 3 to the power of 2.
+print(square(4))        # Print the answer of square 4 to the power of 2.
+print(square(5))        # Print the answer of square 5 to the power of 2.
+
+cube = nth_power(3)     # Passing the exponent value of '3' into the func 'nth_power()', and assigning to the var 'cube'.
+print(cube(2))      # Print the answer of cube 2 to the power of 3.
+print(cube(3))      # Print the answer of cube 3 to the power of 3.
+print(cube(4))      # Print the answer of cube 4 to the power of 3.
+print(cube(5))      # Print the answer of cube 5 to the power of 3.
