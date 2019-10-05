08fc0d49800d40357e56854c5193df7aa522f076
highwind.sg <highwind.sg@gmail.com>
2019-10-05 10:39:50 +0800

    Update module_datetime.py
    update files

--- YouTube_Socratica/module_datetime.py
+++ YouTube_Socratica/module_datetime.py
@@ -15,7 +15,7 @@ print(gvr.year)
 print(gvr.month)
 print(gvr.day)
 
-# The datetime module contains a class named timedelta, whoch allows to add or minus number of days from a date.
+# The datetime module contains a class named timedelta, which allows to add or minus number of days from a date.
 mill = datetime.date(2000, 1, 1)    # To set 2000-01-01 to var 'mill' for millennial .
 print(mill)
 dt = datetime.timedelta(100)
@@ -28,4 +28,62 @@ print(mill + dt)    # This print out of mill + dt, will see that the datetime ch
 print(gvr.strftime("%A, %B %d, %Y"))
 message = "GVR was born on {:%A, %B %d, %Y}."
 print(message.format(gvr))  # Print out the 'message' var in .format() method and pass in the datetime obj of 'gvr'.
+print("")
+
+# SpaceX reused first stage rocket on March 30, 2017 22:27 UTC.
+
+# From datetime module, use the date() class and pass in the params of yyyy, m, dd and assign to var 'launch_date'.
+launch_date = datetime.date(2017, 3, 30)
+# From the datetime module, use the time() class and pass in the params of hh, min, sec and assign to var 'launch_time'.
+launch_time = datetime.time(22, 27, 0)
+# From the datetime module, use the datetime() class and pass in the params of yyyy, m, dd, hh, min, sec to var
+# 'launch_datetime'.
+launch_datetime = datetime.datetime(2017, 3, 30, 22, 27, 0)
+print("")
+
+# Print out the launch date, time and date-time together.
+print(launch_date)
+print(launch_time)
+print(launch_datetime)
+print("")
+
+
+# Can also just obtain the individual hour, minutes or seconds from the 'launch_time' var.
+print(launch_time.hour)
+print(launch_time.minute)
+print(launch_time.second)
+print("")
+
+# Can also just obtain the individual year, month or date from the 'launch_date' var.
+print(launch_date.year)
+print(launch_date.month)
+print(launch_date.day)
+print("")
+
+# Since the full date and time has been passed into var 'launch_datetime', you can also obtain the individual
+# year, month, date, hour, min and secs one at a time.
+print(launch_datetime.year)
+print(launch_datetime.month)
+print(launch_datetime.day)
+print(launch_datetime.hour)
+print(launch_datetime.minute)
+print(launch_datetime.second)
+print("")
+
+# Accessing the current date and time from the datetime module and datetime() class, using the today() method.
+now = datetime.datetime.today()
+print(now)  # This will also print out the current date and time with the last microseconds.
+print(now.microsecond)
+print("")
+
+# Converting strings value to datetime format, using the datetime module and datetime() class and the strptime() method.
+# The 'strptime()' method means to 'strings parse time'. Is used to parse both dates and times.
+moon_landing = "7/20/1969"
+
+# From datetime module, get the datetime() class, use the 'strptime()' method and parse in the params of var obj
+# 'mood_landing' and the assigned value in month, date and year format.
+moon_landing_datetime = datetime.datetime.strptime(moon_landing, "%m/%d/%Y")
+print(moon_landing_datetime)    # This will print out a standard datetime format as an obj, not a string.
+print(type(moon_landing_datetime))  # The output will indeed show an instance of datetime class in a datetime module.
+
 
