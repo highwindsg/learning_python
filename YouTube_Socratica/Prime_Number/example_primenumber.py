00fa0afa22b6c9d15d057ec72ce656f19caa72d9
highwind.sg <highwind.sg@gmail.com>
2019-10-11 13:26:00 +0800

    Update example_primenumber.py
    update file

--- YouTube_Socratica/Prime_Number/example_primenumber.py
+++ YouTube_Socratica/Prime_Number/example_primenumber.py
@@ -12,3 +12,36 @@ Can be factored into smaller integers
 Unit: 1
 (1 is neither called a prime nor composite, it is called a unit)
 """
+
+import time
+import math
+
+def is_prime_v1(n):
+    """Return 'True' if 'n' is a prime number. False, if not a prime number."""
+    if n == 1:
+        return False  # 1 is not a prime number.
+
+    for d in range(2, n):
+        if n % d == 0:
+            return False
+    return True
+
+def is_prime_v2(n):
+    """Return 'True' if 'n' is a prime number. False, if not a prime number."""
+    if n == 1:
+        return False    # 1 is jot a prime
+
+    max_divisor = math.floor(math.sqrt(n))
+    for d in range(2, 1 + max_divisor):
+        if n % d == 0:
+            return False
+    return True
+
+# ===== Test Function =====
+# Below will show which numbers within the range is truly a prime number.
+t0 = time.time()
+for n in range(1, 100000):
+    is_prime_v2(n)
+t1 = time.time()
+print("Time required: ", t1 - t0)
+
