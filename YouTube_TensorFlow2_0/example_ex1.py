b888709f125178ac3632bbceeafcadb88dc2ea18
highwind.sg <highwind.sg@gmail.com>
2019-10-27 11:57:45 +0800

    Update example_ex1.py
    updates

--- YouTube_TensorFlow2_0/example_ex1.py
+++ YouTube_TensorFlow2_0/example_ex1.py
@@ -12,11 +12,11 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
-"""Using the keras API to load in datasets, and the name of the dataset is called fashion_mist."""
+# Using the keras API to load in datasets, and the name of the dataset is called fashion_mnist.
 data = keras.datasets.fashion_mnist
 
 """To prepare some data from the dataset for training and testing. It is not supposed to use the whole dataset
-for training and testing as it takes too long and the result maybe the the program remembers the whole dataset
+for training and testing as it takes too long and the result maybe the program remembers the whole dataset
 and may use it repeatedly for new dataset, which then gives incorrect result."""
 (train_images, train_labels), (test_images, test_labels) = data.load_data()
 
@@ -35,7 +35,8 @@ test_images = test_images / 255.0
 
 # Building model with a sequence of layers.
 model = keras.Sequential([
-    # To flatten the data so that it is passable to the different layers of the neural network.
+    # To flatten the data so that it is passable to the different layers of the neural network. Each image is
+    # represented as 28 by 28 pixels.
     keras.layers.Flatten(input_shape=(28, 28)),
 
     # A dense layer is a fully connected layer, parsing in 128 connected neurons and activation value set to be a
@@ -52,9 +53,27 @@ model = keras.Sequential([
 # The params of optimizer, loss and metrics are arbitrary and will be explained later, or googled later.
 model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
 
-model.fit(train_images, train_labels, epochs=10)  # epochs means how many times the model is going to see the same image
+model.fit(train_images, train_labels, epochs=5)  # epochs means how many times the model is going to see the same image
 # and using epochs needs to be tried a few times to see how many times it will need to train more accurately.
 
 # To see how this model works, we have to test it on the test images and test labels to see how many it gets correct.
 test_loss, test_acc = model.evaluate(test_images, test_labels)
 print("Tested Accuracy:", test_acc)
+
+# Using the model to make predictions.
+# The .predict() method will give you a group of predictions, so must pass in all the test images.
+prediction = model.predict(test_images)
+
+# The output will be all the different probabilities that the neural network has predicted.
+# Since we got 10 ten neurons (T-shirt/top to Ankle boot), this will get the largest value and finds the index of
+# that value.
+# print(class_names[np.argmax(prediction[0])])
+# You will get Ankle boot as the prediction instead of just getting a prediction in number arrays.
+
+# Or you can loop through the prediction and show the images of what is the prediction and what is the actual image.
+for i in range(5):
+    plt.grid(False)
+    plt.imshow(test_images[i], cmap=plt.cm.binary)
+    plt.xlabel("Actual: " + class_names[test_labels[i]])
+    plt.title("Prediction: " + class_names[np.argmax(prediction[i])])
+    plt.show()
