9f34396d97e73483edd286f622ed5dd29f6b4b4f
highwind.sg <highwind.sg@gmail.com>
2019-11-01 06:06:02 +0800

    Update firstGame.py
    update file

--- YouTube_pygame/firstGame.py
+++ YouTube_pygame/firstGame.py
@@ -13,10 +13,13 @@ pygame.display.set_caption("First Game")    # Title of the window.
 
 # Position of the game character and how fast it can move.
 x = 50
-y = 50
+y = 450
 width = 40
 height = 60
 vel = 5     # Velocity speed.
+
+isJump = False
+jumpCount = 10
 
 run = True
 while run:
@@ -28,14 +31,30 @@ while run:
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT]:
+    """Below 'if' statement are for cursor keys movements as well as limiting the border
+    of not allowing the game character to move out of the box."""
+    if keys[pygame.K_LEFT] and x > vel:     # To limit the left/right movement within the box.
         x -= vel    # minus 1 on x coordinate to move left.
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT] and x < 500 - width - vel:      # To limit the left/right movement within the box.
         x += vel    # add 1 on x coordinate to move right.
-    if keys[pygame.K_UP]:
-        y -= vel    # minus 1 from y coordinate to move up.
-    if keys[pygame.K_DOWN]:
-        y += vel    # add 1 to y coordinate to move down.
+    if not(isJump):
+        if keys[pygame.K_UP] and y > vel:       # To limit the top/down movement within the box.
+            y -= vel    # minus 1 from y coordinate to move up.
+        if keys[pygame.K_DOWN] and y < 500 - height - vel:      # To limit the top/down movement within the box.
+            y += vel    # add 1 to y coordinate to move down.
+        # Triggering a jump action for the game character.
+        if keys[pygame.K_SPACE]:
+            isJump = True
+    else:
+        if jumpCount >= -10:
+            neg = 1
+            if jumpCount < 0:
+                neg = -1
+            y -= (jumpCount ** 2) * 0.5 * neg
+            jumpCount -= 1
+        else:
+            isJump = False
+            jumpCount = 10
 
     win.fill((0,0,0))   # To set background color to black.
     pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))   # windows is red color param, and rectangle param.
