23fc38a634329923b42a365caa483afd5e46fd68
highwindsg <highwind.sg@gmail.com>
2019-11-30 15:41:52 +0800

    Update game.py
    update

--- YouTube_PythonGameDev-TextGameRPG/game.py
+++ YouTube_PythonGameDev-TextGameRPG/game.py
@@ -89,6 +89,7 @@ a1 a2 ...   # PLAYER STARTS AT b2
 -------------
 """
 
+ZONENAME = ""
 DESCRIPTION = "description"
 EXAMINATION = "examine"
 SOLVED = False
@@ -97,4 +98,124 @@ DOWN = "down", "south"
 LEFT = "left", "west"
 RIGHT = "right", "east"
 
-solved_places = {}
+solved_places = {"a1": False, "a2": False, "a3": False, "a4": False,
+                "b1": False, "b2": False, "b3": False, "b4": False,
+                "c1": False, "c2": False, "c3": False, "c4": False,
+                "d1": False, "d2": False, "d3": False, "d4": False, 
+                }
+
+zonemap = {
+    "a1": {
+        ZONENAME = "Town Market",
+        DESCRIPTION = "description"
+        EXAMINATION = "examine"
+        SOLVED = False
+        UP = ""
+        DOWN = "b1"
+        LEFT = ""
+        RIGHT = "a2"
+    },
+     "a2": {
+        ZONENAME = "Town Entrance",
+        DESCRIPTION = "description"
+        EXAMINATION = "examine"
+        SOLVED = False
+        UP = ""
+        DOWN = "b2"
+        LEFT = "a1"
+        RIGHT = "a3" 
+    },    
+     "a3": {
+        ZONENAME = "Town Square",
+        DESCRIPTION = "description"
+        EXAMINATION = "examine"
+        SOLVED = False
+        UP = ""
+        DOWN = "b3"
+        LEFT = "a2"
+        RIGHT = "a4" 
+    },    
+     "a4": {
+        ZONENAME = "Town Hall",
+        DESCRIPTION = "description"
+        EXAMINATION = "examine"
+        SOLVED = False
+        UP = ""
+        DOWN = "b4"
+        LEFT = "a3"
+        RIGHT = "" 
+    },    
+     "b1": {
+        ZONENAME = "",
+        DESCRIPTION = "description"
+        EXAMINATION = "examine"
+        SOLVED = False
+        UP = "a1"
+        DOWN = "a1"
+        LEFT = ""
+        RIGHT = "b2"
+    },
+    "b2": {
+        ZONENAME = "Home",
+        DESCRIPTION = "This is your home!"
+        EXAMINATION = "Your home looks the same - nothing has changed."
+        SOLVED = False
+        UP = "a2",
+        DOWN = "c2",
+        LEFT = "b1",
+        RIGHT = "b3",
+    },  
+
+
+}
+
+##### GAME INTERACTIVITY #####
+def print_location():
+    print("\n" + ("#" * (4 + len(myPlayer.location))))
+    print("# " + myPlayer.location.upper() + " #")
+    print("# " + zonemap[myPlayer.position][DESCRIPTION] + " #")
+    print("\n" + ("#" * (4 + len(myPlayer.location))))
+    
+    
+def prompt():
+    print("\n" + "===============================")
+    print("< What would you like to do?)
+    action = input("> ")
+    acceptable_actions = ["move", "go", "travel", "walk", "quit", "examine", "inspect", "look"]
+    while action.lower() not in acceptable_actions:
+        print("Unknown action, try again.\n")
+        action = input("> ")
+    if action.lower() == 'quit':
+        sys.exit()
+    elif action.lower() in ["move", "go", "travel", "walk"]:
+        player_move(action.lower())
+    elif action.lower() in ["examine", "inspect", "interact", "look"]:
+        player_examine(action.lower())
+        
+
+def player_move(myAction):
+    ask = "Where would you like to move to?\n"
+    dest = input(ask)
+    if dest in ["up", "north"]:
+        destination = zonemap[myPlayer.location][UP]
+        movement_handler(destination)
+    elif dest in ["left", "west"]:
+        destination = zonemap[myPlayer.location][LEFT]
+        movement_handler(destination)
+    elif dest in ["east", "right"]:
+        destination = zonemap[myPlayer, location][RIGHT]
+        movement_handler(destination)
+    elif dest in ["south", "down"]:
+        destination = zonemap[myPlayer.location][DOWN]
+        movement_handler(destination)
+
+
+def movement_handler(destination):
+    print("\n" + "You have moved to the " + destination + ".")
+    myPlayer.location = destination
+    print_location()
+    
+
+##### GAME FUNCTIONALITY #####
+def start_game():
+    return
