160c0f2ac19141c393bea18a233578b36d0737d0
highwindsg <38145567+highwindsg@users.noreply.github.com>
2020-02-09 17:40:39 +0800

    Create today.py
    new file

---
+++ HeadFirstPython/today.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import datetime
+
+
+if today == "Saturday":
+    print("Party!!")
+
+elif today == "Sunday":
+    print("Recover.")
+
+else:
+    print("Work, work, work.")
+
