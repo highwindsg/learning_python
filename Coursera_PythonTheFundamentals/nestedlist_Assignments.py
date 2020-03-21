64af019b8da9c69755fc96a699a616fc303e8365
highwindsg <38145567+highwindsg@users.noreply.github.com>
2020-03-21 16:09:15 +0800

    new files
    new files

---
+++ Coursera_PythonTheFundamentals/nestedlist_Assignments.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+
+grades = [["Assignment 1", 80], ["Assignment 2", 90], ["Assignment 3", 70]]
+
+print(len(grades))
+print("")
+
+print(grades[0])
+print(grades[1])
+print(grades[2])
+print("")
+
+for item in grades:
+    print(item)
+print("")
+
+print(grades[0][0])
+print(grades[0][1])
+print(grades[1][0])
+print(grades[1][1])
+print(grades[2][0])
+print(grades[2][1])
+print("")
+
