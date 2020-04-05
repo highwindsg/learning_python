1fa96e9195adcb8ec824b0776583c7693a5aa43b
highwindsg <38145567+highwindsg@users.noreply.github.com>
2020-04-05 11:30:42 +0800

    new files
    new files

---
+++ Coursera_PythonTheFundamentals/nestedlist.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+Q: Using numbers numbers and indexing with non-negative indices,
+write an expression that evaluates to 7. Do not use addition,
+subtraction, or parentheses ( ) (brackets [] are required).
+"""
+
+numbers = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+# Ans:
+numbers[2][0]
+
+# Run lines 9 and 12 in Python interactive mode to see the answer 7.
+
---
+++ Coursera_PythonTheFundamentals/nestedlist2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+Using breakfast and indexing with only negative indices, write an expression
+that evaluates to ’blueberry’. Do not use addition, subtraction or parentheses 
+( ) (brackets [] are required).
+"""
+
+breakfast = [['French', 'toast'], ['blueberry', 'pancakes'],
+             ['scrambled', 'eggs']]
+
+# Ans:
+breakfast[-2][-2]
+
+# Run lines 9 and 12 in Python interactive mode to see the answer 'blueberry'.
+
