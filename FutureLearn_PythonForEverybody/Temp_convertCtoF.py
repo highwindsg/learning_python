464a422905d0f43ccdfb11a167edddad713378e4
highwindsg <38145567+highwindsg@users.noreply.github.com>
2020-05-29 15:27:49 +0800

    new files
    new files

---
+++ FutureLearn_PythonForEverybody/Temp_convertCtoF.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+usersTempC = float(input("Enter a value of degrees Celsius: "))
+convertedTempF = (1.8 * usersTempC) + 32
+print("Converted degrees Celsius to Fahrenheit is: ", convertedTempF)
+
---
+++ FutureLearn_PythonForEverybody/expressions_type.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+width = 17
+height = 12.0
+
+print(type(width//2))
+print(type(width/2.0))
+print(type(height/3))
+print(type(1 + 2 * 5))
+
