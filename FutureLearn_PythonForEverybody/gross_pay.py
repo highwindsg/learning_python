019866d09806006077391026d484060a0f740533
highwindsg <38145567+highwindsg@users.noreply.github.com>
2020-05-29 15:03:18 +0800

    Create gross_pay.py
    new file

---
+++ FutureLearn_PythonForEverybody/gross_pay.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+hours = int(input("Enter hours worked: "))
+rates = float(input("Enter current rates per hours: "))
+Total_Pay = str(hours * rates)
+print("Total salary payment", "$ "+Total_Pay)
+
+
