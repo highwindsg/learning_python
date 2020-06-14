eb5fad4692fbf583f3cdb2e5cc76f1d335b22c63
highwindsg <38145567+highwindsg@users.noreply.github.com>
2020-06-14 10:39:11 +0800

    new files
    new files

---
+++ FutureLearn_PythonForEverybody/if_else.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# No Else statement.
+
+x = 5
+if x < 2:
+    print("Small")
+elif x < 10:
+    print("Medium")
+print("All done")
+
---
+++ FutureLearn_PythonForEverybody/try_except-number.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+rawstr = input("Enter a number: ")
+
+try:
+    ival = int(rawstr)
+except ValueError:
+    ival = -1
+
+if ival > 0:
+    print("Nice work")
+else:
+    print("Not a number")
+
