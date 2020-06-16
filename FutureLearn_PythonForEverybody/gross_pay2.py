b90577cefcb49c0bb131d43264702bd6b6aa56b5
highwindsg <38145567+highwindsg@users.noreply.github.com>
2020-06-16 21:15:23 +0800

    Update gross_pay2.py
    update file

--- FutureLearn_PythonForEverybody/gross_pay2.py
+++ FutureLearn_PythonForEverybody/gross_pay2.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 
-h = float(input("Enter hours: "))
+try:
+    h = float(input("Enter hours: "))   # h for hours worked.
 
-sh = float(40)
+    sh = float(40)  # sh for standard flat hours worked.
 
-r = float(input("Enter current rates per hours: "))
+    r = float(input("Enter current rates per hours: "))     # r for rates.
 
-if h > sh:
-    xtra_hrs = h - sh 
-    xtra_rates = float(r + (r / 2))
+    if h > sh:
+        xtra_hrs = h - sh   # xtra_hrs for additional hours worked.
+        xtra_rates = float(r + (r / 2))     # xtra_rates for additional rates.
 
+except ValueError:
+    print("Non-numeric input detected, please try again.")
+    quit()  # use quit() to stop the program.
 
 Total_Pay = str(sh * r + xtra_hrs * xtra_rates)
 print(Total_Pay)
