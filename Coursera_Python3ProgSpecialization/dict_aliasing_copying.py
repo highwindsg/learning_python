8f953013245020d221c89c952a08cba552d3aac2
highwindsg <38145567+highwindsg@users.noreply.github.com>
2020-07-29 14:56:33 +0800

    Update dict_aliasing_copying.py
    update file

--- Coursera_Python3ProgSpecialization/dict_aliasing_copying.py
+++ Coursera_Python3ProgSpecialization/dict_aliasing_copying.py
@@ -3,7 +3,7 @@
 opposites = {"up": "down", "right": "wrong", "true": "false"}
 alias = opposites
 
-print(alias is opposites)
+print(alias is opposites)   # To check if alias is truly the same as opposites.
 
 alias["right"] = "left"
-print(opposites["right"])
+print(opposites["right"])   # Print out the value of key 'right'.
