dbdf86eee07d986c9aafad9d3b91a2df6e683c4f
highwindsg <38145567+highwindsg@users.noreply.github.com>
2020-08-22 11:34:37 +0800

    Update while_example.py
    updates

--- Coursera_Python3ProgSpecialization/while_example.py
+++ Coursera_Python3ProgSpecialization/while_example.py
@@ -13,3 +13,46 @@ def sumTo(aBound):
 print(sumTo(4))
 print(sumTo(1000))
 print("")
+
+
+"""
+Write a while loop that is initialized at 0 and stops at 15.
+If the counter is an even number, append the counter to a list
+called eve_nums.
+"""
+
+count = 0
+eve_nums = []
+
+while count <= 15:
+    if count %2 == 0:
+        eve_nums.append(count)
+    count += 1
+print(count)
+print("")
+
+"""
+Below is provided a for loop that sums all the elements in list1.
+Write code that accomplishes the same task, but instead use a while
+loop. Assign the accumulator variable to the name accum.
+"""
+
+list1 = [8, 3, 4, 5, 6, 7, 9]
+
+tot = 0 # Assign the accumulator tot with 0 first.
+for elem in list1:
+    tot = tot + elem
+print(tot)
+print("")
+
+""" Alternatively, if using while loop """
+
+idx = 0
+accum = 0
+# print(len(list1))
+while idx < len(list1):
+    accum = accum + list1[idx]
+    idx += 1
+print(accum)
+print("")
+
