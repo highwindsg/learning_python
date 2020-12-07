0334dcab9451e3ff3584a2261479a18352c0dbd0
highwindsg <38145567+highwindsg@users.noreply.github.com>
2020-12-07 17:46:02 +0800

    Update modules_random.py
    updates

--- Coursera_Python3ProgSpecialization/modules_random.py
+++ Coursera_Python3ProgSpecialization/modules_random.py
@@ -10,7 +10,7 @@ diceThrow = random.randrange(1, 20)  # return an int, one of 1-19.
 print(diceThrow)
 print("")
 
-diceThrowAgain = random.randrange(10)   # if you omit the first param (eg. 1, 10) then it is assumed to be 0,
-# and will give you numbers 0-9.
+diceThrowAgain = random.randrange(10)   # if you omit the first param (eg. 1, 10) then it is
+# assumed to be 0, and will give you numbers 0-9.
 print(diceThrowAgain)
 print("")
