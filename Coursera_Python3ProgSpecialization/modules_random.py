95b65e8bbb06524f16263ad42c10964cc0c3b434
highwindsg <38145567+highwindsg@users.noreply.github.com>
2020-12-07 17:44:28 +0800

    new files
    new files

---
+++ Coursera_Python3ProgSpecialization/modules_random.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import random
+
+prob = random.random()
+print(prob)
+print("")
+
+diceThrow = random.randrange(1, 20)  # return an int, one of 1-19.
+print(diceThrow)
+print("")
+
+diceThrowAgain = random.randrange(10)   # if you omit the first param (eg. 1, 10) then it is assumed to be 0,
+# and will give you numbers 0-9.
+print(diceThrowAgain)
+print("")
