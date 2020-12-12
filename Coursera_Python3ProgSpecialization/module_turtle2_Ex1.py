e57a917789bd80e0464dc5ce15ec488d6ecc09c8
highwindsg <38145567+highwindsg@users.noreply.github.com>
2020-12-12 15:52:38 +0800

    updates
    updates

--- Coursera_Python3ProgSpecialization/module_turtle2_Ex1.py
+++ Coursera_Python3ProgSpecialization/module_turtle2_Ex1.py
@@ -9,12 +9,23 @@ color of the window according to the user’s wishes.
 https://www.w3schools.com/colors/colors_names.asp. 
 It includes some quite unusual ones, like “PeachPuff” and “HotPink”.)
 
-Do similar changes to allow the user, at runtime, to set tess’ color.
+Do similar changes to allow the user, at runtime, to set alex’s color.
 
-Do the same for the width of tess’ pen. Hint: your dialog with the user
-will return a string, but tess’ pensize method expects its argument to
+Do the same for the width of alex's pen. Hint: your dialog with the user
+will return a string, but alex’s pensize method expects its argument to
 be an int. That means you need to convert the string to an int before
 you pass it to pensize.
 
 Then finally run the program which draws a rectangle.
 """
+
+import turtle
+
+
+wn = turtle.Screen()
+wn.bgcolor = input("Please enter the background color:", wn.bgcolor)
+
+alex = turtle.Turtle()
+alex.color = input("Please enter the color of turtle alex:", alex.color)
+
+#... unfinish....
