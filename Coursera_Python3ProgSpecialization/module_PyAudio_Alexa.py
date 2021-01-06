48a30aaf3a1413d85e11582358314bc5d3935046
highwindsg <38145567+highwindsg@users.noreply.github.com>
2021-01-06 20:50:28 +0800

    update file
    update file

--- Coursera_Python3ProgSpecialization/module_PyAudio_Alexa.py
+++ Coursera_Python3ProgSpecialization/module_PyAudio_Alexa.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 # https://www.youtube.com/watch?v=AWvsXxDtEkU
+# Note: Run the script in a separate terminal window.
+
 
 import speech_recognition as sr
 import pyttsx3
@@ -13,26 +15,60 @@ import pyjokes
 listener = sr.Recognizer() # To create a listening recognizer as set var.
 engine = pyttsx3.init() # To initialize this text-to-speech engine.
 voices = engine.getProperty("voices")
-engine.setProperty("voices", voices[4].id)   # voices index 4 is female id voice for Alexa.
-engine.say("Hi! I'm Alexa, your IT Space special agent.")
-engine.say("What can I do for you?")
-engine.runAndWait()
+engine.setProperty("voice", voices[4].id)   # voices index 4 is female id voice for Alexa.
 
-print(sr.Microphone.list_microphone_names())
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+#print(sr.Microphone.list_microphone_names())
 # Set up a try and except block just in case microphone is not receiving well so that the
 # program can try to listen again.
 
-try:
-    with sr.Microphone() as source: # Using microphone as a source,
-        # listener.adjust_for_ambient_noise(source)
-        listener.adjust_for_ambient_noise(source,duration=1)
-        print("Listening....")
-        voice = listener.listen(source, timeout=10)    # then calling the speech_recognizer to listen to this source.
-        command = sr.recognize_google(voice) #  Using the listener's google API, and pass in the voice.
-        command = command.lower()
-        if 'alexa' in command:
-            print(command)
+def take_command():
+    try:
+        with sr.Microphone() as source: # Using microphone as a source,
+            #listener.adjust_for_ambient_noise(source)
+            #listener.adjust_for_ambient_noise(source,duration=1)
+            print("Listening....")
+            #voice = listener.listen(source, timeout=10)    # then calling the speech_recognizer to listen to this source.
+            voice = listener.listen(source)    # then calling the speech_recognizer to listen to this source.
+            command = listener.recognize_google(voice) #  Using the listener's google API, and pass in the voice.
+            command = command.lower()
+            #if 'alexa' in command:
+            if '' in command:
+                #command = command.replace("alexa", "")
+                command = command.replace("", "")
+                print("You said:", command)
+
+    except:
+        pass
+    return command
+
+def run_alexa():
+    command = take_command()
+    #print(command)
+    if "play" in command:
+        song = command.replace("play", "")
+        talk("Playing " + song)
+        #print(song)
+        pywhatkit.playonyt(song)
+    elif "time" in command:
+        time = datetime.datetime.now().strftime("%I:%M %p")
+        print(time)
+        talk("Current time is " + time)
+    elif "who is" in command:
+        person = command.replace("who is", "")
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif "what is" in command:
+        thing = command.replace("what is", "")
+        info = wikipedia.summary(thing, 1)
+        print(info)
+        talk(info)
+    elif "joke" in command:
+        talk(pyjokes.get_joke())
 
 
-except:
-    pass
+run_alexa()
