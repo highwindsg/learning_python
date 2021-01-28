874e3fd64aa7cc90cc5c5b1358824114d64ae408
highwindsg <38145567+highwindsg@users.noreply.github.com>
2021-01-28 16:29:30 +0800

    new files
    new files

---
+++ Coursera_Python3ProgSpecialization/list_accumulation1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+accum = 0   # initializing
+
+for item in nums:   # iterating
+    accum = accum + item    # updating
+
+print(accum)
+print("")
