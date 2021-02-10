8315e9f0dd0531b69ae6a2a7b7788a65391d3476
highwindsg <38145567+highwindsg@users.noreply.github.com>
2021-02-10 10:01:36 +0800

    new files
    new files

--- Coursera_Python3ProgSpecialization/for_loop2.py
+++ Coursera_Python3ProgSpecialization/for_loop2.py
@@ -76,6 +76,24 @@ the color, and the fill color of a regular polygon. The program should draw the
 polygon and then fill it in.
 """
 
-num_sides = input("Enter the number of sides of the polygon: ")
+import turtle
+
+wn = turtle.Screen()
+alex = turtle.Turtle()
+
+num_sides = int(input("Enter the number of sides of the polygon: "))
 length_sides = float(input("Enter the length of the side(cm): "))
-color = int(input("Enter the color codes of the polygon: "))
+colorname = input("Color of polygon? ")
+fcolor = input("Fill color of polygon? ")
+
+alex.color(colorname)
+alex.fillcolor(fcolor)
+alex.begin_fill()
+
+for i in range(int(num_sides)):
+    alex.forward(float(length_sides))
+    alex.left(int(360 / num_sides))
+
+
+alex.end_fill()
+wn.exitonclick()
