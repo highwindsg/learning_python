27de3b518deff2ed32a26ab0025066e3d2f865e4
highwindsg <38145567+highwindsg@users.noreply.github.com>
2021-02-10 14:00:24 +0800

    new files
    new files

--- Coursera_Python3ProgSpecialization/for_loop2.py
+++ Coursera_Python3ProgSpecialization/for_loop2.py
@@ -68,32 +68,3 @@ for item in num_list:
     print(item, item*item)
 print("")
 
-
-
-"""
-Write a program that asks the user for the number of sides, the length of the side, 
-the color, and the fill color of a regular polygon. The program should draw the 
-polygon and then fill it in.
-"""
-
-import turtle
-
-wn = turtle.Screen()
-alex = turtle.Turtle()
-
-num_sides = int(input("Enter the number of sides of the polygon: "))
-length_sides = float(input("Enter the length of the side(cm): "))
-colorname = input("Color of polygon? ")
-fcolor = input("Fill color of polygon? ")
-
-alex.color(colorname)
-alex.fillcolor(fcolor)
-alex.begin_fill()
-
-for i in range(int(num_sides)):
-    alex.forward(float(length_sides))
-    alex.left(int(360 / num_sides))
-
-
-alex.end_fill()
-wn.exitonclick()
