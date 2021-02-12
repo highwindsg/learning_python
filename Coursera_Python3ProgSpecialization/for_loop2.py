a0870cbf9c77121d9d3b99864ab0e1f6cf789843
highwindsg <38145567+highwindsg@users.noreply.github.com>
2021-02-12 22:22:26 +0800

    new files
    new files

--- Coursera_Python3ProgSpecialization/for_loop2.py
+++ Coursera_Python3ProgSpecialization/for_loop2.py
@@ -222,10 +222,6 @@ Write code to create a list of numbers from 0 to 67 and assign that list to the 
 Do not hard code the list.
 """
 
-nums = []
-
-for i in range(0, 68):
-
-    
+nums = list(range(0, 68))
 print(nums)
 print("")
