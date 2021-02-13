52e5545d32f88a64b40dca6032cf60482fd953f8
highwindsg <38145567+highwindsg@users.noreply.github.com>
2021-02-13 17:26:30 +0800

    Create booleansValues_and_Expressions.py
    new file

---
+++ Coursera_Python3ProgSpecialization/booleansValues_and_Expressions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+print(True and True)
+print("")
+
+print(True and False)
+print("")
+
+total = 0
+
+print(total == 0 and 3 < 4)
+print("")
+
+print(total == 0 and 3 > 4)
+print("")
+
+print(not total == 0)
+print("")
