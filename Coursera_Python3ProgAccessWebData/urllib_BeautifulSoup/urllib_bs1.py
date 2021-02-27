a3c6b91ca720543f297dff47a8713061fb1f6fff
highwindsg <38145567+highwindsg@users.noreply.github.com>
2021-02-27 16:04:35 +0800

    Update urllib_bs1.py
    updates

--- Coursera_Python3ProgAccessWebData/urllib_BeautifulSoup/urllib_bs1.py
+++ Coursera_Python3ProgAccessWebData/urllib_BeautifulSoup/urllib_bs1.py
@@ -6,14 +6,12 @@ from bs4 import BeautifulSoup   # From module 'bs4', import 'BeautifulSoup' func
 
 url = input("Enter URL: ")  # Enter the input for URL and assign in to the var 'url'.
 html = urllib.request.urlopen(url).read()   # Using the .urlopen() method, parse in the
-# url values, and read it using the .read() method. The assign the values to var 'html'.
-soup = BeautifulSoup(html, "html.parser")   # Using the 'BeautifulSoup()' func, parse
-# in the two arguments of var 'html' and 'html.parser' option, and assign to var 'soup'.
-# The option 'html.parser' sort of cleans up the html.
+# url values, and read it using the .read() method.
+soup = BeautifulSoup(html, "html.parser")   # Using the 'BeautifulSoup()
 
 # Retrieve all of the anchor tags.
 tags = soup("a")
-#print(tags)
+print(tags)
 print("")
 
 for tag in tags:
