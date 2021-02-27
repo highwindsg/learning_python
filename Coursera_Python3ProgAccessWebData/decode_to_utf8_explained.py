f8d2981f2191f5d22ddab3404dc293dc38eec0d1
highwind <highwind.sg@gmail.com>
2021-02-27 11:55:51 +0800

    updates

--- Coursera_Python3ProgAccessWebData/decode_to_utf8_explained.py
+++ Coursera_Python3ProgAccessWebData/decode_to_utf8_explained.py
@@ -5,10 +5,10 @@ import socket
 """
     Python Strings to Bytes
     =======================
-    1. When we talk to an external resource like a network socket se sends bytes,
+    1. When we talk to an external resource like a network socket we sends bytes,
     so we need to encode Python 3 strings into a given character encoding.
     2. When we read data from an external resource, we must decode it based on
-    the character set so it is properly represented in Python3 as a string.
+    the character set, so it is properly represented in Python 3 as a string.
 """
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Connects the socket.
