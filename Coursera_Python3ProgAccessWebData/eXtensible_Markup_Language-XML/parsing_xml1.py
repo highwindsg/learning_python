d070c88515022ec0549582ad5384ba7ac184926b
highwind <highwind.sg@gmail.com>
2021-03-03 12:59:46 +0800

    new files
    new files

--- Coursera_Python3ProgAccessWebData/eXtensible_Markup_Language-XML/parsing_xml1.py
+++ Coursera_Python3ProgAccessWebData/eXtensible_Markup_Language-XML/parsing_xml1.py
@@ -21,14 +21,14 @@ tree = ET.fromstring(data)
 
 # From var obj 'tree' use the .find() method and parse in the 'name' tag and
 # get it's text by appending '.text'. And then print it out.
-print("Name:",tree.find("name").text)
+print("Name:", tree.find("name").text)
 
 # From var obj 'tree' use the .find() method and parse in the 'phone' tag and
 # display it's type value by using the '.get()' method. And then print it out.
 print(tree.find("phone").get("type"))
-print("phone:",tree.find("phone").text)
+print("phone:", tree.find("phone").text)
 
 # From var obj 'tree' use the .find() method and parse in the 'email' tag and
 # display the attrib of 'hide' by appending '.get()' method parse in 'hide'.
-print("Attr:",tree.find("email").get("hide"))
+print("Email hidden?:", tree.find("email").get("hide"))
 print("")
---
+++ Coursera_Python3ProgAccessWebData/eXtensible_Markup_Language-XML/parsing_xml2.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+""" A triple single quoted 'string' (eg. ''' ... ... ''') in Python is a multi-line 
+'string' type.
+Below explains how to pull out data from xml.
+"""
+
+import xml.etree.cElementTree as ET
+
+
+input = '''<stuff>
+    <users>
+        <user x = "2">
+            <id>001</id>
+            <name>Chuck</name>
+        </user>
+        <user x = "7">
+            <id>009</id>
+            <name>Brent</name>
+        </user>
+    </users>
+</stuff>'''
+
+print(input)
+print("")
+print(type(input))
+print("")
+
+# Parse the entire xml triple single quoted string 'input' obj into var
+# obj 'stuff'.
+stuff = ET.fromstring(input)
+
+# From 'stuff', use the .findall() method and parse in all the 'user' in the 
+# 'users' tag, and assign to the var 'lst' as a list.
+lst = stuff.findall("users/user")
+print(lst)
+print("")
+
+print("User count:", len(lst))
+
+for item in lst:
+    # From the item, use the .find() method and parse in the 'name' tag and
+    # display it's value by appending the '.text' option. And print out the Name.
+    print("Name", item.find("name").text)
+    
+    # From the item, use the .find() method and parse in the 'id' tag and
+    # display it's value by appending the '.text' option. And print out the Id.
+    print("Id", item.find("id").text)
+    
+    # From the item, use the .get() method and parse in the attrib of 'x' 
+    # so as to get the value of attrib 'x'. And print out the Attribute.
+    print("Attribute", item.get("x"))
+    
+print("")
