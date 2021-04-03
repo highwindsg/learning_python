3a7575dd39c3c08ebd5344ddbd3fae474113b37a
highwindsg <38145567+highwindsg@users.noreply.github.com>
2021-04-03 16:10:44 +0800

    new files
    new files

---
+++ if_statem_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+myColor = input("Please Enter Your Color: ")
+if myColor == "Red":
+    print("Your Color is Red")
+if myColor != "Red":
+    print("Your Color is NOT Red")
+
