7c203488961336377e929a7ef053d5620adf8c32
highwindsg <38145567+highwindsg@users.noreply.github.com>
2021-04-18 21:00:26 +0800

    Create CompoundConditional.py
    new file

---
+++ CompoundConditional.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+## https://www.youtube.com/watch?v=E-c0w5Latbc
+
+
+myColor = input("Please input your color: ")
+if myColor == "Red" or myColor == "red" or myColor == "RED":
+    print("Your color is red")
+if myColor != "Red" and myColor != "red" and myColor != "RED":
+    print("Your color is not red")
+    
