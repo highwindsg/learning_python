376b3ccabe4dd7835fc059b14a95557cd7b99733
highwindsg <38145567+highwindsg@users.noreply.github.com>
2021-04-19 22:00:26 +0800

    new files
    new files

---
+++ if_CompoundConditional2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+myColor = input("Please input your color: ")
+if myColor.lower() == "red":
+    print("The color is red.")
+else:
+    print("The color is not red.")
---
+++ if_CompoundConditional3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+number = float(input("Please enter your number between 5 and 10: "))
+
+if number >= 5 and number <= 10:
+    print("Correct, the entered number", number, "is indeed between 5 and 10.")
+else:
+    print("Number entered in not between 5 and 10.")
+    
+    
