fea26093af276ca00a7cf636159f245c318b81e3
highwindsg <38145567+highwindsg@users.noreply.github.com>
2021-04-21 11:41:00 +0800

    Update MouseMovement.py
    update file.

--- PythonAutoGui/MouseMovement.py
+++ PythonAutoGui/MouseMovement.py
@@ -2,13 +2,12 @@
 
 # https://www.youtube.com/watch?v=DzQXk9jesPc
 
-import pyautogui
-import time
+import pyautogui    # Import module 'pyautogui'.
+import time # Import module 'time'.
 
 
-pyautogui.FAILSAFE = False
+pyautogui.FAILSAFE = False  # From pyautogui module, set the FAILSAFE attrib to False.
 while True:
-    time.sleep(15)
-    for i in range(0, 100):
-        pyautogui.moveTo(0, i * 5)
-        
+    time.sleep(15)  # From time module, set the a sleep timer to 15 secs.
+    for i in range(0, 100): # For item in range 0-99,
+        pyautogui.moveTo(0, i * 5)  # move mouse cursor.
