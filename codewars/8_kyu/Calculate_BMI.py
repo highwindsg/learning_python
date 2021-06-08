dcc526846f32b22be225bca8bffab9fe1ce91871
highwindsg <38145567+highwindsg@users.noreply.github.com>
2021-06-08 21:12:29 +0800

    Create Calculate_BMI.py
    new file

---
+++ codewars/8_kyu/Calculate_BMI.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+"""
+Write function bmi that calculates body mass index (bmi = weight / height2).
+
+if bmi <= 18.5 return "Underweight"
+
+if bmi <= 25.0 return "Normal"
+
+if bmi <= 30.0 return "Overweight"
+
+if bmi > 30 return "Obese"
+"""
+
+def bmi(w, h):
+    b = w / h ** 2
+     
+    if b <= 18.5:
+        return "Underweight"
+            
+    elif b <= 25.0:
+        return "Normal"
+            
+    elif b <= 30.0:
+        return "Overweight"
+            
+    else:   # therefore any numbers greater than 30, will be obese.
+        return "Obese"
+
+
+# Alternate clever solution
+"""
+def bmi(weight, height):
+    b = weight / height ** 2
+    return ['Underweight', 'Normal', 'Overweight', 'Obese'][(b > 30) + (b > 25) + (b > 18.5)]
+"""
+
+print(bmi(50, 1.80))
+print(bmi(80, 1.80))
+print(bmi(90, 1.80))
+print(bmi(110, 1.80))
+print(bmi(50, 1.50))
+    
+print("")
