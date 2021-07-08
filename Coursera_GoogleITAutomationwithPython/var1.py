f1d965f7c5a1689ed460d7f79d672621f3b6baab
highwindsg <38145567+highwindsg@users.noreply.github.com>
2021-07-09 07:41:41 +0800

    new file
    new file

---
+++ Coursera_GoogleITAutomationwithPython/var1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+Calculate the area of a triangle of base 5, height 3 and output the result.  
+Reminder:  the area of a triangle is (base*height)/2.
+"""
+
+base = 5
+height = 3
+area = (base * height)/2
+
+print(area)
+print("")
