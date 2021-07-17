8043c4fe27adcd7e29a12b5ed3482b516a523524
highwindsg <38145567+highwindsg@users.noreply.github.com>
2021-07-17 15:27:38 +0800

    Create functions6_comparison_if.py
    new file

---
+++ Coursera_GoogleITAutomationwithPython/functions6_comparison_if.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+The is_positive function should return True if the number received is positive, otherwise 
+it returns None. Can you fill in the gaps to make that happen?
+Remember that positive numbers are those bigger than zero.
+"""
+
+def is_positive(number):
+    if number >= 1:
+        return True
+    
+
+print(is_positive(0))
+print("")
