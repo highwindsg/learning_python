b3608bcee8b992a64a2cc6dee4ac780ee41ed47a
highwindsg <38145567+highwindsg@users.noreply.github.com>
2021-07-26 16:52:58 +0800

    new file
    new file

--- Coursera_GoogleITAutomationwithPython/whileloop1.py
+++ Coursera_GoogleITAutomationwithPython/whileloop1.py
@@ -44,3 +44,29 @@ Note: Therefore whenever you're writing a loop, check that you're initializing a
 variables you want to use before you use them.
 """
 
+
+
+"""
+The following code causes an infinite loop.
+Can you figure out what’s missing and how to fix it?
+
+def print_range(start, end):
+    # Loop through the numbers from start to end
+    n = start
+    while n <= end:
+        print(n)
+        
+print_range(1, 5)  # Should print 1 2 3 4 5 (each number on its own line)
+"""
+
+def print_range(start, end):
+    # Loop through the numbers from start to end
+    n = start
+    while n <= end:
+        print(n)
+        n += 1  # Therefore the answer is to include this line to increase the accumulator
+        # until 'n' reaches lesser or equal to the value of the second param (end).
+
+
+print_range(1, 5)  # Should print 1 2 3 4 5 (each number on its own line) 
+print("")
