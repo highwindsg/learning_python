7fc083238323e2ff2ff122f3e41b69ae15ee6322
highwindsg <38145567+highwindsg@users.noreply.github.com>
2021-07-26 10:54:57 +0800

    Create whileloop1.py
    new file

---
+++ Coursera_GoogleITAutomationwithPython/whileloop1.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+x = 0
+while x < 5:
+    print("Not there yet, x = " + str(x))
+    x += 1
+    
+#print(x)
+print("x = " + str(x) + ", so we stop here.")
+print("")
+
+
+def attempts(n):
+    x = 1
+    while x <= n:
+        print("Attempts " + str(x))
+        x += 1
+    
+    print("Done")
+    
+    
+attempts(5)
+print("")
+
+
+
+"""
+In this code, there's an initialization problem that's causing our function to behave 
+incorrectly. Can you find the problem and fix it?
+"""
+
+def count_down(start_number):
+    current = start_number  # Therefore answer is 'current = start_number'.
+    while (current > 0):
+        print(current)
+        current -= 1
+    print("Zero!")
+
+
+count_down(4)
+print("")
+"""
+Note: Therefore whenever you're writing a loop, check that you're initializing all the
+variables you want to use before you use them.
+"""
+
