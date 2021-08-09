33577d50168aa85b9156b05be97afacd955714ee
highwindsg <38145567+highwindsg@users.noreply.github.com>
2021-08-09 09:30:49 +0800

    new files
    new files

---
+++ Kaggle_30DaysofML/Day2/least_difference-help-Docstrings.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+def least_difference(a, b, c):
+    """Return the smallest difference between any two numbers
+    among a, b and c.
+    
+    >>> least_difference(1, 5, -5)
+    4
+    """
+
+    diff1 = abs(a - b)
+    diff2 = abs(b - c)
+    diff3 = abs(a - c)
+    return min(diff1, diff2, diff3)
+
+
+print(
+    least_difference(1, 10, 100),
+    least_difference(1, 10, 10), # Python allows trailing commas in argument lists.
+    least_difference(5, 6, 7)
+)
+print("")
+
+
+
+"""
+Using the help() func to seek help to understand about a func you have forgotten.
+help(function_name)
+"""
+
+help(least_difference)
