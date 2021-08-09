fd16958c45058aca38bf27b954c7260fcb3b04de
highwindsg <38145567+highwindsg@users.noreply.github.com>
2021-08-09 11:54:34 +0800

    new files
    new files

--- Kaggle_30DaysofML/Day2/functions_to_functions.py
+++ Kaggle_30DaysofML/Day2/functions_to_functions.py
@@ -14,7 +14,7 @@ def call(fn, arg):
 
 
 def squared_call(fn, arg):
-    """Xall fn on the result of calling fn on arg"""
+    """Call fn on the result of calling fn on arg"""
     return fn(fn(arg))
 
 
@@ -22,5 +22,26 @@ print(
     call(mult_by_five, 1),
     squared_call(mult_by_five, 1),
     sep="\n" # '\n' is a newline character - it starts a new line.
+)
+#print("")
+
+
+
+"""
+Passing in a function using the optional keyword 'key=' argument, when doing
+a function call.
+"""
+
+def mod_5(x):
+    """Return the remainder of x afrer dividing by 5"""
+    return x % 5
+
+
+print(
+    "Which number is biggest?",
+    max(100, 51, 14),
+    "Which number is the biggest modulo 5?",
+    max(100, 51, 14, key=mod_5), # Passing in the func 'mod_5()' as an optional arg.
+    sep="\n"
 )
 print("")
